d6c88dc69f46611dc55b563b1b0bcce3faef5adf
tery <vladislavutkin92@gmail.com>
2019-08-08 18:34:17 +0300

    завершено

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,12 +1,24 @@
-document.getElementById("cookie").onclick = () => {
+const timeLog = document.getElementById("clicker__counter_speed"),
+    cookies = document.getElementById("cookie");
+
+let times = 0;
+cookies.onclick = () => {
     document.getElementById("clicker__counter").textContent++;
     document.body.style.backgroundColor = colorRandom();
+    timeLog.textContent = (1000 / (Date.now() - times)).toFixed(1);
+    times = Date.now();
 }
-function colorRandom(){
-        const r=Math.floor(Math.random() * (256));
-        const g=Math.floor(Math.random() * (256));
-        const b=Math.floor(Math.random() * (256));
-        const c='#' + r.toString(16) + g.toString(16) + b.toString(16);
+cookies.onmousedown = ()=> {
+    cookies.width = 490;
+    cookies.heigth = 490;
+}
+cookies.onmouseup = ()=> {
+    cookies.width = 500;
+    cookies.heigth = 500;
+};
+function colorRandom() {
+        const r=Math.floor(Math.random() * (256)), g=Math.floor(Math.random() * (256)),b=Math.floor(Math.random() * (256)),
+        c='#' + r.toString(16) + g.toString(16) + b.toString(16);
         return c;
 }
     
