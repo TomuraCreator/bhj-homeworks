9e07181536730518a8a2eb60e3a281fb332cce5a
tery <vladislavutkin92@gmail.com>
2019-08-15 19:38:21 +0300

    исправлена вторая задача

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,18 @@
 const links = document.querySelectorAll('.menu__link'),
     menu = document.querySelectorAll('.menu');
 
-function mouseHover() {
-    let element = this.nextElementSibling;
-    if(!element) return
-    else element.style.display = 'block';
-    element.addEventListener('mouseleave', function(){
-        element.style.display = 'none'
+for(let i = 0; i< links.length; i++) {
+    links[i].addEventListener('click', function(event) {
+        let elem = this.nextElementSibling;
+        if(!elem) return //если детей нет выходим из функции
+        else {
+            event.preventDefault();
+            if(!elem.classList.contains('menu_active')) {
+                for(let b = 0; b < menu.length; b++) {
+                    menu[b].classList.remove('menu_active');
+                } // если во всложенном списке нет класса menu_active, удаляем этот класс в других местах
+            }
+            elem.classList.toggle('menu_active') //иначе открываем список 
+        }
     })
-}
-
-for(let i = 0; i<links.length; i++) {
-    links[i].addEventListener('mouseenter', mouseHover)
 }
