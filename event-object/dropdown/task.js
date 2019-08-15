b4beba135bb9e6459fa866df52fef6d6245b6426
tery <vladislavutkin92@gmail.com>
2019-08-15 21:34:38 +0300

    добавлено описание сущностей

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,15 +1,15 @@
-const dropDown = document.querySelector('.dropdown__value'),
-    dropdown_list = document.querySelector('.dropdown__list'), 
-    list = dropdown_list.childNodes;
+const dropDown = document.querySelector('.dropdown__value'),//кнопка 
+    dropdown_list = document.querySelector('.dropdown__list'), //список
+    list = dropdown_list.childNodes; // пункты списка
 
 dropDown.addEventListener('click', ()=> {
     dropdown_list.classList.toggle('dropdown__list_active')
-})
+}) // раскрытие закрытие списка по нажатию кнопки
+
 for(let i = 0; i < list.length; i++) {
     list[i].addEventListener('mousedown', function() {
         event.preventDefault();
         dropDown.textContent = event.target.textContent;
-        console.log(event.target.textContent)
         dropdown_list.classList.remove('dropdown__list_active');
     })
-}
+}// выбор, замена текста на кнопке, закрытие списка
