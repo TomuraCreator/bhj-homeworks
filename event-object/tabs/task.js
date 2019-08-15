e64d0de4f42f349980434d1c14a5a473fea62ff0
tery <vladislavutkin92@gmail.com>
2019-08-15 22:27:12 +0300

    решена задача 3

---
+++ event-object/tabs/task.js
@@ -0,0 +1,13 @@
+const main = document.querySelectorAll('.tab'),
+    tab_content = document.querySelectorAll('.tab__content');
+
+for(let i = 0; i < main.length; i++) {
+    main[i].addEventListener('click', () => {
+        for(let b = 0; b < main.length; b++) {
+            main[b].classList.remove('tab_active');
+            tab_content[b].classList.remove('tab__content_active');
+        }
+        main[i].classList.add('tab_active');
+        tab_content[i].classList.add('tab__content_active')
+    })
+}
