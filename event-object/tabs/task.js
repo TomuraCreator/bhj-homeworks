03fa7e0a2c5ad169afbc69397e46977f6fac625a
tery <vladislavutkin92@gmail.com>
2019-08-15 22:31:45 +0300

    вынесена функция очистки

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,11 +3,15 @@ const main = document.querySelectorAll('.tab'),
 
 for(let i = 0; i < main.length; i++) {
     main[i].addEventListener('click', () => {
-        for(let b = 0; b < main.length; b++) {
-            main[b].classList.remove('tab_active');
-            tab_content[b].classList.remove('tab__content_active');
-        }
+        clearClass(); // очищаем классы
         main[i].classList.add('tab_active');
         tab_content[i].classList.add('tab__content_active')
     })
-}
+} 
+
+function clearClass() {
+    for(let b = 0; b < main.length; b++) {
+        main[b].classList.remove('tab_active');
+        tab_content[b].classList.remove('tab__content_active');
+    }
+} // удаляет классы активаторы
