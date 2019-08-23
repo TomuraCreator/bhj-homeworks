a1d966f298e2d7e99a56c06bcfb3c9c64d4cfd8f
tery <vladislavutkin92@gmail.com>
2019-08-23 18:44:31 +0300

    задача доработана

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,7 +1,6 @@
 const block = document.querySelector('.reveal');
-
 window.addEventListener('scroll', ()=> {
     let block_size = block.getBoundingClientRect();
-    if(block_size.top < 1300) block.classList.add('reveal_active')
+    if (block_size.top < window.innerHeight) block.classList.add('reveal_active')
     else block.classList.remove('reveal_active')
 })
